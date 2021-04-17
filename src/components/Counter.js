d60d0f6f218c6220b60d79e8683de9e6f102d5fc
Eli <ezsto92@gmail.com>
2021-04-17 10:49:26 -0400

    working counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,38 @@
+import { useState } from "react";
 import "./Counter.css";
 
 function Counter() {
+  const [count, setCount] = useState(5);
+
+  function handleReset() {
+    //use setCount to modify state at runtime
+    setCount(0);
+  }
+  function handleUp() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  // JSX always comes last
   return (
     <div className="Counter">
       <h3>Counter</h3>
-      <p>0</p>
+      <p>{count}</p>
       <p>
-        <button type="button">Down</button>
-        <button type="button">Reset</button>
-        <button type="button">Up</button>
+        {/* This is a way to put the function in the JSX directly */}
+        <button
+          type="button"
+          onClick={() => {
+            setCount((prev) => prev - 1);
+          }}
+        >
+          Down
+        </button>
+        <button type="button" onClick={handleReset}>
+          Reset
+        </button>
+        <button type="button" onClick={handleUp}>
+          Up
+        </button>
       </p>
     </div>
   );
