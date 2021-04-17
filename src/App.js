df077f21b3ff1a8739139d468580bf7cd47c5fa1
Eli <ezsto92@gmail.com>
2021-04-17 10:12:12 -0400

    start of switch, non functional

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Counter from "./components/Counter";
+import Switch from "./components/Switch";
 
 function App() {
   return (
     <div className="App">
       <h1>This is React</h1>
       <Counter />
+      <Switch />
     </div>
   );
 }
---
+++ src/components/Switch.js
@@ -0,0 +1,17 @@
+import "./Switch.css";
+
+function Switch() {
+  return (
+    <div className="Switch">
+      <h3>Switch</h3>
+      <p>On</p>
+      <p>Off</p>
+      <p>
+        <button type="button">On</button>
+        <button type="button">Off</button>
+        <button type="button">Toggle</button>
+      </p>
+    </div>
+  );
+}
+export default Switch;
