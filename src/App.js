6fbc38bff7d9e7c95f228723be39560e82ceb4d3
Eli <ezsto92@gmail.com>
2021-04-18 09:28:39 -0400

    changed to brackets for temp

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <h1>This is React</h1>
-      <Weather time="today" conditions="Overcast" tempF="45" />
+      <Weather time="today" conditions="Overcast" tempF={45} />
       <Counter />
       <Switch />
       <Votes />
