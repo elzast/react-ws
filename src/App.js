6f1a34a2f059db7facad80fc8c7857b69ccb4020
Eli <ezsto92@gmail.com>
2021-04-18 09:26:12 -0400

    Weather component up to date

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import Counter from "./components/Counter";
 import Switch from "./components/Switch";
 import Votes from "./components/Votes";
+import Weather from "./components/Weather";
 
 function App() {
   return (
     <div className="App">
       <h1>This is React</h1>
+      <Weather time="today" conditions="Overcast" tempF="45" />
       <Counter />
       <Switch />
       <Votes />
